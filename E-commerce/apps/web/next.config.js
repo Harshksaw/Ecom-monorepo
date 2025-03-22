719001015d4bef2eb26102a87073553cf4e3e70d
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-03-21 19:13:34 -0700

    feat(assets): reorganize and add new assets including images and fonts

--- E-commerce/apps/web/next.config.js
+++ E-commerce/apps/web/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['i.ibb.co','lh3.googleusercontent.com','res.cloudinary.com'],
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
